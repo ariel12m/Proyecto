937a3fbe245b7a3d617510874d138c4a5b4fd6e3
Erick Sebastián <esprado@sudamericano.edu.ec>
2026-06-02 16:35:10 -0500

    Reimport services and controllers as modules (clean code)

--- backend-events/src/app.module.ts
+++ backend-events/src/app.module.ts
@@ -1,34 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaService } from './prisma/prisma.service';
 import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './auth/jwt.strategy';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
-import { EventService } from './event/event.service';
-import { EventController } from './event/event.controller';
 import { EventModule } from './event/event.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
+    PrismaModule,
+    UserModule,
+    EventModule,
     ConfigModule.forRoot({ isGlobal: true }),
     JwtModule.registerAsync({
       useFactory: () => ({
         secret: process.env.JWT_SECRET,
-        signOptions: { expiresIn: '1h' }
-      })
+        signOptions: { expiresIn: '1h' },
+      }),
     }),
-    EventModule
   ],
-  controllers: [AuthController, UserController, EventController],
-  providers: [
-    PrismaService,
-    AuthService,
-    JwtStrategy,
-    UserService,
-    EventService,
-  ]
+  controllers: [AuthController, AppController],
+  providers: [AuthService, JwtStrategy, AppService],
 })
 export class AppModule {}
