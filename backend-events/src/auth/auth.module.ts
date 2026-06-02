1fe48837987f5c6d4870ecdfca09a5d17ffecad0
Erick Sebastián <esprado@sudamericano.edu.ec>
2026-06-02 16:36:42 -0500

    Use relative routes

--- backend-events/src/auth/auth.module.ts
+++ backend-events/src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [
