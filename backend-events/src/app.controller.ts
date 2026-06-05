6376d4f42c2c77a04476d91bd307ed9b3ba0e213
Erick Sebastián <esprado@sudamericano.edu.ec>
2026-06-05 12:21:02 -0500

    Eggino easterino

--- backend-events/src/app.controller.ts
+++ backend-events/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { NoResponse } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  get(): Promise<NoResponse> {
+    return this.appService.no();
   }
 }
