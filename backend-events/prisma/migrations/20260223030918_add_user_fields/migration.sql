600e423b04c18dd3ca282dad7e26aea8f47cc38f
Erick Sebastián <esprado@sudamericano.edu.ec>
2026-06-05 11:07:26 -0500

    Delete Prisma migrations

--- backend-events/prisma/migrations/20260223030918_add_user_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "address" TEXT,
-ADD COLUMN     "cedula" TEXT,
-ADD COLUMN     "licenseNumber" TEXT,
-ADD COLUMN     "phone" TEXT,
-ADD COLUMN     "vehicle" TEXT;
